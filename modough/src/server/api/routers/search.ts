807518981ae20f98c8fd205a62e4f080fdf6b84f
Mike Peterson <mike@lemonshell.com>
2024-04-27 16:33:14 -0600

    migrate

--- modough/src/server/api/routers/search.ts
+++ modough/src/server/api/routers/search.ts
@@ -6,6 +6,7 @@ export const searchRoute = createTRPCRouter({
   search: publicProcedure
     .input(z.object({ address: z.string().min(1) }))
     .mutation(async ({ input }) => {
+      
       return {
         greeting: `Hello ${input.address}`,
       };
