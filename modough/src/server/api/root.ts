745109320f86fcdbbf15ef691fb1700e2d97836b
Mike Peterson <mike@lemonshell.com>
2024-04-27 14:46:13 -0600

    start basic page

--- modough/src/server/api/root.ts
+++ modough/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { searchRoute } from "./routers/search";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  search: searchRoute,
 });
 
 // export type definition of API
---
+++ modough/src/server/api/routers/search.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const searchRoute = createTRPCRouter({
+  search: publicProcedure
+    .input(z.object({ address: z.string().min(1) }))
+    .mutation(async ({ input }) => {
+      return {
+        greeting: `Hello ${input.address}`,
+      };
+    }),
+});
