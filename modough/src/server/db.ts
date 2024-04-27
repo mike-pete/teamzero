7d9e793776992b50564c71bd5bb5334999c0d24d
Mike Peterson <mike@lemonshell.com>
2024-04-27 15:08:13 -0600

    add turso db

--- modough/src/server/db.ts
+++ modough/src/server/db.ts
@@ -1,9 +1,18 @@
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
-
 import { env } from "~/env";
+
+const libsql = createClient({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
 
 const createPrismaClient = () =>
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
