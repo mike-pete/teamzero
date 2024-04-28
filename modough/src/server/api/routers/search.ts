51983489da4e89b2d391757aa621d67d75c2b21c
Mike Peterson <mike@lemonshell.com>
2024-04-27 18:54:26 -0600

    remove unused

--- modough/src/server/api/routers/search.ts
+++ modough/src/server/api/routers/search.ts
@@ -4,14 +4,15 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { db } from "~/server/db";
 
 export const searchRoute = createTRPCRouter({
-  getStops: publicProcedure.query(async () => {}),
+  getStops: publicProcedure.query(async () => {
+    const stops = await db.busStops.findMany();
+
+    console.log("stops", stops);
+    return stops;
+  }),
   search: publicProcedure
     .input(z.object({ address: z.string().min(1) }))
     .mutation(async ({ input }) => {
-      const stops = await db.busStops.findMany();
-
-      console.log("stops", stops);
-      return stops;
       return {
         greeting: `Hello ${input.address}`,
       };
